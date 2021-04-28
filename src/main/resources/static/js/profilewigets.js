e7c7e85e151963fd0158e0268ea0c349606324b2
Luka Kresoja <57074484+LukeOnuke@users.noreply.github.com>
2021-04-28 23:01:19 +0200

    Create profilewigets.js

---
+++ src/main/resources/static/js/profilewigets.js
@@ -0,0 +1,10 @@
+/*
+@since 1.0.1a
+Use id or class to select img and p elements and fill them with profile info.
+https://www.w3schools.com/jsref/met_document_getelementsbyclassname.asp
+Ex.
+
+<img class="profilewiget-pi"></img> //Will get profile image.
+<p class="profilewiget-fin"></p> //Will get user first name.
+<p class="profilewiget-fan"></p> //Will get user family name.
+*/
